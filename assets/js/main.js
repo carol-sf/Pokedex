988dee5a7873a10a9ad80a34b715f208a67d3466
carol-sf <annacarolina.franca21@gmail.com>
2023-05-13 00:53:25 -0300

    refatorando e renomeando meu script principal

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+const pokemonList = document.querySelector("#pokemonList");
+const limit = 5;
+let offset = 0;
+
+function loadPokemons(offset, limit) {
+    pokeApi.getPokemons(offset,limit).then((pokemonListJson = []) => {
+        const newHtml = pokemonListJson.map((pokemon) => `
+            <li class="pokemon ${pokemon.type}">
+                <span class="numero">#${pokemon.number}</span>
+                <span class="nome">${pokemon.name}</span>
+                <div class="detalhes">
+                    <ol class="tipos">
+                        ${pokemon.types.map((type) => `<li>${type}</li>`).join('')}
+                    </ol>
+                    <img src="${pokemon.photo}"
+                        alt="${pokemon.name}">
+                </div>
+            </li>
+        `).join('');
+    
+        pokemonList.innerHTML += newHtml;
+    });
+}
+
+loadPokemons(offset, limit);
