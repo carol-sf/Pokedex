58083ef1599af233cc207a980a2aa81dc22f6a38
carol-sf <annacarolina.franca21@gmail.com>
2023-05-19 13:17:43 -0300

    adicionado link para detalhes nos pokemons

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,17 +8,19 @@ let offset = 0;
 function carregarPokemons(offset, limit) {
     pokeApi.getPokemons(offset,limit).then((pokemonListJson = []) => {
         const novoHtml = pokemonListJson.map((pokemon) => `
-            <li class="pokemon ${pokemon.tipo}">
-                <span class="numero">#${pokemon.numero}</span>
-                <span class="nome">${pokemon.nome}</span>
-                <div class="detalhes">
-                    <ol class="tipos">
-                        ${pokemon.tipos.map((tipo) => `<li>${tipo}</li>`).join('')}
-                    </ol>
-                    <div class="foto">
-                        <img src="${pokemon.foto}" alt="${pokemon.nome}">
+            <li>
+                <a href="detalhes.html#${pokemon.nome}" class="pokemon ${pokemon.tipo}">
+                    <span class="numero">#${pokemon.numero}</span>
+                    <span class="nome">${pokemon.nome}</span>
+                    <div class="detalhes">
+                        <ol class="tipos">
+                            ${pokemon.tipos.map((tipo) => `<li>${tipo}</li>`).join('')}
+                        </ol>
+                        <div class="foto">
+                            <img src="${pokemon.foto}" alt="${pokemon.nome}">
+                        </div>
                     </div>
-                </div>
+                </a>      
             </li>
         `).join('');
     
